0835a11c700e3f6476f1cd95d3e74edf8684d9e7
Jakob <Jakob.hyldgaard@gmail.com>
2019-12-05 12:43:20 +0100

    rettelser

--- opret_opgave.c
+++ opret_opgave.c
@@ -32,7 +32,7 @@ task create_task(task tasks[], int *new_task){
              &tasks[*new_task].deadline.tm_mday,
              &month,
              &year);
-        
+
     } while (tasks[*new_task].deadline.tm_hour < 0 || tasks[*new_task].deadline.tm_hour > 23 ||
              tasks[*new_task].deadline.tm_min  < 0 || tasks[*new_task].deadline.tm_min  > 59 ||
              tasks[*new_task].deadline.tm_mday < 1 || tasks[*new_task].deadline.tm_mday > 31 ||
@@ -41,33 +41,37 @@ task create_task(task tasks[], int *new_task){
     tasks[*new_task].deadline.tm_mon  = month - 1;
     tasks[*new_task].deadline.tm_year = year  - 1900;
 
-    /* Print den givne information, så brugeren kan se om der skal ændres noget */
-    printf("\nOpgave %d vil blive oprettet med følgende information:\n"
-           "%-26s%s \n%-26s%s \n%-26s%s \n%-26s%s \n%-26s%s \n%-26s%d "
-           "\n%-26s%02d.%02d %02d.%02d.%d\n",
-           *new_task,
-           "Titel: ", tasks[*new_task].title,
-           "Administrerende personer: ", tasks[*new_task].admins,
-           "Frivillige personer: ", tasks[*new_task].volunteers,
-           "Beskrivelse: ", tasks[*new_task].description,
-           "Status: ", tasks[*new_task].status_str,
-           "Prioritering: ", tasks[*new_task].priority,
-           "Deadline: ",
-           tasks[*new_task].deadline.tm_hour, tasks[*new_task].deadline.tm_min,
-           tasks[*new_task].deadline.tm_mday, tasks[*new_task].deadline.tm_mon + 1,
-           tasks[*new_task].deadline.tm_year + 1900);
-    
+    print_task(tasks[task]);
+
     /* Spørg om der skal ændres noget */
     printf("Vil du ændre noget i opgaven? [Ja/Nej]:\n");
     do {
         scanf(" %s", answer);
     } while (strcmp(answer, "ja") && strcmp(answer, "Ja") && strcmp(answer, "JA") &&
              strcmp(answer, "nej") && strcmp(answer, "Nej") && strcmp(answer, "NEJ"));
-    
+
     /* Hvis der skal ændres noget så gør funktionen til at ændre en opgave */
     if (strcmp(answer, "ja") && strcmp(answer, "Ja") && strcmp(answer, "JA")){
-        /* Edit task */ 
+        /* Edit task */
     }
     (*new_task) += 1;
     return tasks[(*new_task) - 1];
+}
+
+void print_task(task task1){
+    /* Print den givne information, så brugeren kan se om der skal ændres noget */
+    printf("\nOpgave %d vil blive oprettet med følgende information:\n"
+           "%-26s%s \n%-26s%s \n%-26s%s \n%-26s%s \n%-26s%s \n%-26s%d "
+           "\n%-26s%02d.%02d %02d.%02d.%d\n",
+           task1.number,
+           "Titel: ", tasks1.title,
+           "Administrerende personer: ", tasks1.admins,
+           "Frivillige personer: ", tasks1.volunteers,
+           "Beskrivelse: ", tasks1.description,
+           "Status: ", tasks1.status_str,
+           "Prioritering: ", tasks1.priority,
+           "Deadline: ",
+           tasks1.deadline.tm_hour, tasks1.deadline.tm_min,
+           tasks1.deadline.tm_mday, tasks1.deadline.tm_mon + 1,
+           tasks1.deadline.tm_year + 1900);
 }
--- opret_opgave.h
+++ opret_opgave.h
@@ -1,2 +1,3 @@
 
 task create_task(task tasks[], int *new_task);
+void print_task(task task1);
