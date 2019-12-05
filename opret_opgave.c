2a328a0108b8fae2aa70866a26a814241b9290cf
Mourits Jørgensen <mouritsjorgensen@h442.aau1x.klient.audt.site.aau.dk>
2019-12-05 12:47:13 +0100

    change to opret

--- opret_opgave.c
+++ opret_opgave.c
@@ -1,45 +1,46 @@
 #include "include.h"
 
 /* En funktion til at oprettet en opgave i systemet */
-task create_task(task tasks[], int *new_task){
-    int month, year;
+void create_task(task tasks[], int *new_task){
+    int month, year, i = *new_task;
     char answer[10];
+    *new_task += 1;
 
-    tasks[*new_task].number = (*new_task) + 1;
+    tasks[i].number = i + 1;
     /* Spørg om alt det nødvendige information til at oprette en opgave */
     printf("Indtast de administrerende personer til opgaven - afslut med enter [Max 250 tegn]:\n");
-    scanf(" %[^\n]", tasks[*new_task].admins);
+    scanf(" %[^\n]", tasks[i].admins);
     printf("Indtast titlen til opgaven - afslut med enter [Max 100 tegn]:\n");
-    scanf(" %[^\n]", tasks[*new_task].title);
+    scanf(" %[^\n]", tasks[i].title);
     printf("Beskriv opgaven - afslut med } og enter [Max 1000 tegn]:\n");
-    scanf(" %[^}]%*c", tasks[*new_task].description);
+    scanf(" %[^}]%*c", tasks[i].description);
     printf("Indtast de frivillige personer til opgaven - afslut med enter [Max 250 tegn]:\n");
-    scanf(" %[^\n]", tasks[*new_task].volunteers);
+    scanf(" %[^\n]", tasks[i].volunteers);
     printf("Indtast den nuværende status på opgaven - afslut med enter [Max 1000 tegn]:\n");
-    scanf(" %[^\n]", tasks[*new_task].status_str);
+    scanf(" %[^\n]", tasks[i].status_str);
     printf("Indtast prioriteringen af opgaven - afslut med enter [fra 1 - 10]:\n");
     do {
-        scanf(" %d", &tasks[*new_task].priority);
-        if (tasks[*new_task].priority < 1 || tasks[*new_task].priority > 10){
+        scanf(" %d", &tasks[i].priority);
+        if (tasks[i].priority < 1 || tasks[i].priority > 10){
             printf("Tallet var ikke mellem 1 og 10, indtast prioritering igen:\n");
         }
-    } while (tasks[*new_task].priority < 1 || tasks[*new_task].priority > 10);
+    } while (tasks[i].priority < 1 || tasks[i].priority > 10);
     printf("Indtast deadline på formen timer.minutter dato.måned.år - afslut med enter [fx 17.00 09.12.2019]:\n");
     do {
         scanf(" %d.%d %d.%d.%d",
-             &tasks[*new_task].deadline.tm_hour,
-             &tasks[*new_task].deadline.tm_min,
-             &tasks[*new_task].deadline.tm_mday,
+             &tasks[i].deadline.tm_hour,
+             &tasks[i].deadline.tm_min,
+             &tasks[i].deadline.tm_mday,
              &month,
              &year);
 
-    } while (tasks[*new_task].deadline.tm_hour < 0 || tasks[*new_task].deadline.tm_hour > 23 ||
-             tasks[*new_task].deadline.tm_min  < 0 || tasks[*new_task].deadline.tm_min  > 59 ||
-             tasks[*new_task].deadline.tm_mday < 1 || tasks[*new_task].deadline.tm_mday > 31 ||
+    } while (tasks[i].deadline.tm_hour < 0 || tasks[i].deadline.tm_hour > 23 ||
+             tasks[i].deadline.tm_min  < 0 || tasks[i].deadline.tm_min  > 59 ||
+             tasks[i].deadline.tm_mday < 1 || tasks[i].deadline.tm_mday > 31 ||
              month < 1 || month  > 12 ||
              year  < 0 || year   > 2100);
-    tasks[*new_task].deadline.tm_mon  = month - 1;
-    tasks[*new_task].deadline.tm_year = year  - 1900;
+    tasks[i].deadline.tm_mon  = month - 1;
+    tasks[i].deadline.tm_year = year  - 1900;
 
     print_task(tasks[task]);
 
@@ -54,8 +55,6 @@ task create_task(task tasks[], int *new_task){
     if (strcmp(answer, "ja") && strcmp(answer, "Ja") && strcmp(answer, "JA")){
         /* Edit task */
     }
-    (*new_task) += 1;
-    return tasks[(*new_task) - 1];
 }
 
 void print_task(task task1){
--- opret_opgave.h
+++ opret_opgave.h
@@ -1,3 +1,3 @@
 
-task create_task(task tasks[], int *new_task);
+void create_task(task tasks[], int *new_task);
 void print_task(task task1);
