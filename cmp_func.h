d47c5e14f91df9e3aa38ea81d413cd307e66a5d0
Mourits Jørgensen <mouritsjorgensen@h48.aau1x.klient.audt.site.aau.dk>
2019-12-06 09:44:49 +0100

    Added compare function to new file

---
+++ cmp_func.h
@@ -0,0 +1,16 @@
+/* Compare function */
+
+/* qsort sammenligning for deadlines */
+int sort_deadline(const void *ip1, const void *ip2);
+
+/* qsort sammenligning for priority */
+int sort_priority(const void *ip1, const void *ip2);
+
+/* qsort sammenligning for admins */
+int sort_admins(const void *ip1, const void *ip2);
+
+/* qsort sammenligning for title */
+int sort_title(const void *ip1, const void *ip2);
+
+/* qsort sammenligning for category */
+int sort_category(const void *ip1, const void *ip2);
