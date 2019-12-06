14015f9eb88dbcee457d46a8316553f667e0a55b
Daniel Held-Hansen <dheldh19@student.aau.dk>
2019-12-06 10:17:18 +0100

    Update til sort_print

--- print_sort.c
+++ print_sort.c
@@ -32,34 +32,34 @@ void print_sort(task tasks[], int option, int number_of_tasks){
         case category:
             printf("Sorteret for Kategori:\n");
             for (i=0; i < number_of_tasks; i++){
-                printf("%s: %d\n%s: %s\n%s: %s\n\n", "Nummer", tasks[i].number, "Kategori", tasks[i].category, "Titel", tasks[i].title);
+                printf("%s: %d\n%s: %s\n%s: %s\n\n", "Nummer", i+1, "Kategori", tasks[i].category, "Titel", tasks[i].title);
             }
             break;
         case title:
             printf("Sorteret for Titel:\n");
             printf("%-10s %-10s\n", "Nummer:", "Titel");
             for (i=0; i < number_of_tasks; i++){
-                printf("%-10d %-s\n", tasks[i].number, tasks[i].title);
+                printf("%-10d %-s\n", i+1, tasks[i].title);
             }
             break;
         case admins:
             printf("Sorteret for Ansvarlig:\n");
             for (i=0; i < number_of_tasks; i++){
-               printf("%s: %d\n%s: %s\n%s: %s\n\n", "Nummer", tasks[i].number, "Ansvarlig", tasks[i].admins, "Titel", tasks[i].title);
+               printf("%s: %d\n%s: %s\n%s: %s\n\n", "Nummer", i+1, "Ansvarlig", tasks[i].admins, "Titel", tasks[i].title);
             }
             break;
         case priority:
             printf("Sorteret for Prioritering:\n");
             printf("%-10s %-14s %-10s\n", "Nummer:", "Prioritering:", "Titel:");
             for (i=0; i < number_of_tasks; i++){
-                printf("%-10d %-14d %-s\n", tasks[i].number, tasks[i].priority, tasks[i].title);
+                printf("%-10d %-14d %-s\n", i+1, tasks[i].priority, tasks[i].title);
             }
             break;
         case deadline:
             printf("Sorteret for Deadline:\n");
             printf("%-10s %-17s %-10s\n", "Nummer:","Deadline:" ,"Titel:");
             for (i=0; i < number_of_tasks; i++){
-                printf("%-10d %02d.%02d %02d.%02d.%5d %-s\n", tasks[i].number, tasks[i].deadline.tm_hour,
+                printf("%-10d %02d.%02d %02d.%02d.%5d %-s\n", i+1, tasks[i].deadline.tm_hour,
                 tasks[i].deadline.tm_min, tasks[i].deadline.tm_mday, tasks[i].deadline.tm_mon + 1, tasks[i].deadline.tm_year + 1900, tasks[i].title);
             }
             break;
