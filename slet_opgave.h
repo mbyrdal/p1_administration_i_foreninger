039ae61ff18bfe4e9d69976d42c46bc1a6c6b5fd
Jakob <Jakob.hyldgaard@gmail.com>
2019-12-10 18:01:34 +0100

    deleted is_yes_or_no and is_no

--- slet_opgave.h
+++ slet_opgave.h
@@ -1,4 +1,2 @@
 void delete_task(task tasks[], int *number_of_tasks);
-int user_wrote_no(char *string);
-int is_yes_or_no(char *string);
 void delete_category(task tasks[], int *number_of_tasks, char category[]);
