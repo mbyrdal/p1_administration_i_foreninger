aa8d0b8bc251de5c4f93111acd621a19c3d7dbb3
Daniel Held-Hansen <dheldh19@student.aau.dk>
2019-12-11 15:06:36 +0100

    Fikset ændre opgave

--- opgave_func.c
+++ opgave_func.c
@@ -66,7 +66,8 @@ void create_task(task tasks[], char categories[MAX_NUMBER_OF_CATEGORIES][MAX_LEN
  */
 void change_task(task tasks[], char categories[MAX_NUMBER_OF_CATEGORIES][MAX_LENGTH_OF_CATEGORY], int number_of_tasks, int *number_of_categories, int index){
     enum option {Title = 1, Category, Admins, Volunteers, Description, Status, Priority, Deadline};
-    int option = prompt_user_options("\nHvad vil du gerne aendre? [indtast valgmulighedens nummer uden punktum \".\"]: \n\n"
+    do {
+        int option = prompt_user_options("\nHvad vil du gerne aendre? [indtast valgmulighedens nummer uden punktum \".\"]: \n\n"
                                      "1.  Titel\n"
                                      "2.  Kategori\n"
                                      "3.  Administrerende personer\n"
@@ -75,67 +76,69 @@ void change_task(task tasks[], char categories[MAX_NUMBER_OF_CATEGORIES][MAX_LEN
                                      "6.  Status\n"
                                      "7.  Prioritet\n"
                                      "8.  Deadline\n"
+                                     "9.  Afslut aendringer"
                                      "> ",
                                      8);
 
-    switch (option){
-        case Title:
-            printf("Indtast titlen til opgaven - afslut med enter [Max 100 tegn]:\n");
-            scanf(" %[^\n]", tasks[index].title);
-            break;
+        switch (option){
+            case Title:
+                printf("Indtast titlen til opgaven - afslut med enter [Max 100 tegn]:\n");
+                scanf(" %[^\n]", tasks[index].title);
+                break;
 
-        case Category:
-            prompt_for_category(tasks, categories, number_of_tasks, number_of_categories, index);
-            break;
+            case Category:
+                prompt_for_category(tasks, categories, number_of_tasks, number_of_categories, index);
+                break;
 
-        case Admins:
-            printf("Indtast de administrerende personer til opgaven - afslut med enter [Max 250 tegn]:\n");
-            scanf(" %[^\n]", tasks[index].admins);
-            break;
+            case Admins:
+                printf("Indtast de administrerende personer til opgaven - afslut med enter [Max 250 tegn]:\n");
+                scanf(" %[^\n]", tasks[index].admins);
+                break;
 
-        case Volunteers:
-            printf("Indtast de frivillige personer til opgaven - afslut med enter [Max 250 tegn]:\n");
-            scanf(" %[^\n]", tasks[index].volunteers);
-            break;
+            case Volunteers:
+                printf("Indtast de frivillige personer til opgaven - afslut med enter [Max 250 tegn]:\n");
+                scanf(" %[^\n]", tasks[index].volunteers);
+                break;
 
-        case Description:
-            printf("Beskriv opgaven - afslut med } og enter [Max 1000 tegn]:\n");
-            scanf(" %[^}]%*c", tasks[index].description);
-            break;
+            case Description:
+                printf("Beskriv opgaven - afslut med } og enter [Max 1000 tegn]:\n");
+                scanf(" %[^}]%*c", tasks[index].description);
+                break;
 
-        case Status:
-            printf("Indtast den nuvaerende status paa opgaven - afslut med } og enter [Max 1000 tegn]:\n");
-            scanf(" %[^}]%*c", tasks[index].status_str);
-            break;
+            case Status:
+                printf("Indtast den nuvaerende status paa opgaven - afslut med } og enter [Max 1000 tegn]:\n");
+                scanf(" %[^}]%*c", tasks[index].status_str);
+                break;
 
-        case Priority:
-            printf("Indtast prioriteringen af opgaven - afslut med enter [fra 1 - 10]:\n");
-            do {
-                scanf(" %d", &tasks[index].priority);
-                clear_input();
-                if (tasks[index].priority < 1 || tasks[index].priority > 10){
-                    printf("Tallet var ikke mellem 1 og 10, indtast prioritering igen:\n");
-                }
-            } while (tasks[index].priority < 1 || tasks[index].priority > 10);
-            break;
+            case Priority:
+                printf("Indtast prioriteringen af opgaven - afslut med enter [fra 1 - 10]:\n");
+                do {
+                    scanf(" %d", &tasks[index].priority);
+                    clear_input();
+                    if (tasks[index].priority < 1 || tasks[index].priority > 10){
+                        printf("Tallet var ikke mellem 1 og 10, indtast prioritering igen:\n");
+                    }
+                } while (tasks[index].priority < 1 || tasks[index].priority > 10);
+                break;
 
-        case Deadline:
-            printf("Indtast deadline paa formen timer.minutter dato.maaned.aar - afslut med enter [fx 17.00 09.12.2019]:\n");
-            do {
-                scanf(" %d.%d %d.%d.%d",
-                    &tasks[index].deadline.tm_hour,
-                    &tasks[index].deadline.tm_min,
-                    &tasks[index].deadline.tm_mday,
-                    &tasks[index].deadline.tm_mon,
-                    &tasks[index].deadline.tm_year);
+            case Deadline:
+                printf("Indtast deadline paa formen timer.minutter dato.maaned.aar - afslut med enter [fx 17.00 09.12.2019]:\n");
+                do {
+                    scanf(" %d.%d %d.%d.%d",
+                        &tasks[index].deadline.tm_hour,
+                        &tasks[index].deadline.tm_min,
+                        &tasks[index].deadline.tm_mday,
+                        &tasks[index].deadline.tm_mon,
+                        &tasks[index].deadline.tm_year);
 
-            } while (tasks[index].deadline.tm_hour < 0 || tasks[index].deadline.tm_hour > 23 ||
-                     tasks[index].deadline.tm_min  < 0 || tasks[index].deadline.tm_min  > 59 ||
-                     tasks[index].deadline.tm_mday < 1 || tasks[index].deadline.tm_mday > 31 ||
-                     tasks[index].deadline.tm_mon  < 1 || tasks[index].deadline.tm_mon  > 12 ||
-                     tasks[index].deadline.tm_year < 0 || tasks[index].deadline.tm_year > 2100);
-            break;
-    }
+                } while (tasks[index].deadline.tm_hour < 0 || tasks[index].deadline.tm_hour > 23 ||
+                        tasks[index].deadline.tm_min  < 0 || tasks[index].deadline.tm_min  > 59 ||
+                        tasks[index].deadline.tm_mday < 1 || tasks[index].deadline.tm_mday > 31 ||
+                        tasks[index].deadline.tm_mon  < 1 || tasks[index].deadline.tm_mon  > 12 ||
+                        tasks[index].deadline.tm_year < 0 || tasks[index].deadline.tm_year > 2100);
+                break;
+        }
+    } while (option != 9);
 }
 
 /* Funktion til at spørge om en kategori, som eksisterer i categories-arrayet.
