da5459f01c2e33d58d76c0d5d43eea0970583fb4
Simon Holm <56428011+ssholm@users.noreply.github.com>
2019-12-11 14:51:34 +0100

    Formatering af output

--- main_menu.c
+++ main_menu.c
@@ -45,7 +45,7 @@ void execute_user_input(task tasks[], char categories[MAX_NUMBER_OF_CATEGORIES][
                                                         "> ", *number_of_tasks) - 1;
                 change_task(tasks, categories, *number_of_tasks, number_of_categories, change_task_index);
             } else{
-                printf("Kan ikke aendre naar der ingen opgaver er!\n");
+                printf("\nKan ikke aendre naar der ingen opgaver er!\n");
             }
             break;
 
@@ -59,7 +59,7 @@ void execute_user_input(task tasks[], char categories[MAX_NUMBER_OF_CATEGORIES][
                     print_task(tasks[view_task_index]);
                 }
             } else{
-                printf("Kan ikke vise opgaverne naar der ingen opgaver er!\n");
+                printf("\nKan ikke vise opgaverne naar der ingen opgaver er!\n");
             }
             break;
 
@@ -67,7 +67,7 @@ void execute_user_input(task tasks[], char categories[MAX_NUMBER_OF_CATEGORIES][
             if (*number_of_tasks > 0){
                 delete_task(tasks, number_of_tasks);
             } else{
-                printf("Kan ikke slette naar der ingen opgaver er!\n");
+                printf("\nKan ikke slette naar der ingen opgaver er!\n");
             }   
             break;
 
@@ -85,7 +85,7 @@ void execute_user_input(task tasks[], char categories[MAX_NUMBER_OF_CATEGORIES][
                 }
                 *number_of_categories -= 1;
             } else{
-                printf("Kan ikke slette en kategori naar der ingen er!\n");
+                printf("\nKan ikke slette en kategori naar der ingen er!\n");
             }
             break;
             
